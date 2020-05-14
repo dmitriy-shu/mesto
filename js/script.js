ddad1331aa6f901b67b7d355439e86e69437a1d4
dmitriy-shu <shumilin-dma@yandex.ru>
2020-05-14 19:45:56 +0300

    Проектная работа 4 rew.2

--- js/script.js
+++ js/script.js
@@ -7,17 +7,11 @@ const profileAbout = document.querySelector('.profile__subtitle');
 const popupName = document.querySelector('.popup__input-name');
 const popupAbout = document.querySelector('.popup__input-about');
 
-let name;
-let about;
-
-
 
 function popupOpened() {
   popup.classList.add('popup_opened');
-  name = profileName.textContent;
-  about = profileAbout.textContent;
-  popupName.setAttribute('value', name);
-  popupAbout.setAttribute('value', about);
+  popupName.value = profileName.textContent;
+  popupAbout.value = profileAbout.textContent;
 };
 editButton.addEventListener('click', popupOpened);
 
@@ -31,11 +25,8 @@ let formElement = document.querySelector('.popup__container');
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  name = popupName.value;
-  about = popupAbout.value;
-  profileName.textContent = name;
-  profileAbout.textContent = about;
-
+  profileName.textContent = popupName.value;
+  profileAbout.textContent = popupAbout.value;
   popup.classList.remove('popup_opened');
 };
 
