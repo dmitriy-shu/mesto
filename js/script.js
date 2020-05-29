2fc70314245859daadf18a901f56a7b4cdb64430
dmitriy-shu <shumilin-dma@yandex.ru>
2020-05-29 11:41:45 +0300

    Созданы начальные карточки и форма добавления

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ const popupAbout = document.querySelector('.popup__input-about');
 
 const formElement = document.querySelector('.popup__container');
 
-const card = document.querySelector('.card-grid');
-const itemCardTemplate = document.querySelector('.card-template').content;
 
+//  Форма редактирования профиля 
 
+// открыть
 function popupOpened() {
   popup.classList.add('popup_opened');
   popupName.value = profileName.textContent;
@@ -20,13 +20,14 @@ function popupOpened() {
 };
 editButton.addEventListener('click', popupOpened);
 
+// закрыть
 function popupClose() {
   popup.classList.remove('popup_opened');
 };
 closeButton.addEventListener('click', popupClose);
 
 
-
+// редактировать профиль
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
@@ -36,7 +37,11 @@ function formSubmitHandler(evt) {
 };
 formElement.addEventListener('submit', formSubmitHandler);
 
+
 //  Добавляем начальные карточки
+
+const card = document.querySelector('.card-grid');
+const itemCardTemplate = document.querySelector('.card-template').content;
 
 const initialCards = [
   {
@@ -65,7 +70,7 @@ const initialCards = [
   }
 ];
 
-
+// добавить начальные каррточки
 function renderCardTemplate(item) {
 
   const cardTemplate = itemCardTemplate.cloneNode(true);
@@ -79,8 +84,46 @@ function render() {
   initialCards.forEach(renderCardTemplate)
 }
 
-
-
 render()
 
+//  Форма добавления карточки
+
+const addButton = document.querySelector('.profile__add-button');
+const addCloseButton = document.querySelector('.popup-add__icon-close');
+const addPopup = document.querySelector('.popup-add');
+const addPopupPlace = document.querySelector('.popup-add__input-place');
+const addPopupLink = document.querySelector('.popup-add__input-link')
+
+// открыть
+function addPopupOpened() {
+  addPopup.classList.add('popup_opened');
+}
+addButton.addEventListener('click', addPopupOpened);
+
+// закрыть
+function addPopupClose() {
+  addPopup.classList.remove('popup_opened');
+}
+addCloseButton.addEventListener('click', addPopupClose);
+
+
+// добавить карточку
+const addformElement = document.querySelector('.popup-add__container');
+function formSubmitAdd(evt) {
+  evt.preventDefault();
+  if (addPopupPlace.value && addPopupLink.value) {
+    console.log(addPopupPlace.value, addPopupLink.value);
+    const cardUser = itemCardTemplate.cloneNode(true);
+    cardUser.querySelector('.card__title').textContent = addPopupPlace.value;
+    cardUser.querySelector('.card__image').setAttribute('src', addPopupLink.value);
+    cardUser.querySelector('.card__image').setAttribute('alt', addPopupPlace.value);
+    card.prepend(cardUser);
+    addPopup.classList.remove('popup_opened');
+    addPopupPlace.value = '';
+    addPopupLink.value = '';
+  }
+
+
+};
+addformElement.addEventListener('submit', formSubmitAdd);
 
