1d4454a8b03ca3cd98dd95e6f356a9ba6aab0767
dmitriy-shu <shumilin-dma@yandex.ru>
2020-07-07 13:28:32 +0300

    Проектная работа 8. Продолжение рефакторинга

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { Card } from './class.js';
+import { Card } from './card.js';
 import { FormValidator } from './validate.js';
 
 const editButton = document.querySelector('.profile__edit-button');
